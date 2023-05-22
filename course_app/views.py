d6536a1472d88a572faab30c9eacc1cbd7427107
bibekghorasainee <ghorasainibidit@gmail.com>
2023-05-22 13:17:54 +0545

    filter rows

--- course_app/views.py
+++ course_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from course_app.models import Course
 
 # Create your views here.
 def home(request):
+    c=Course.objects.all()
+    print("""""""""""""""""")
+    print(c)
+    d=Course.objects.filter(title='python')
+    print(d)
     return render(request, 'course_app/home.html')
